3102ea016d8cb795aee50383362ba422dc594027
Abel <contacto@abelcabezaroman.com>
2019-03-06 20:42:12 +0100

    remove min for now

--- src/dics.js
+++ src/dics.js
@@ -10,6 +10,7 @@
  *
  */
 (function (root) {
+
     /**
      *
      * @type {{container: null, filters: null, hideTexts: null, textPosition: string, linesOrientation: string, rotate: number}}
@@ -22,69 +23,69 @@
         linesOrientation: 'horizontal',
         rotate: 0
     };
+
     /**
      *
      * @param options
      * @constructor
      */
-
     function Dics(options) {
         this.options = utils.extend({}, [defaultOptions, options], {
             clearEmpty: true
         });
+
+
         this.container = this.options.container;
-
         this._setOrientation(this.options.linesOrientation, this.container);
-
-        this.images = this._getImages();
-        this.sliders = [];
+        this.images        = this._getImages();
+        this.sliders       = [];
         this._activeSlider = null;
 
         if (this.container == null) {
-            console.error('Container element not found!');
+            console.error('Container element not found!')
         }
 
         this._build();
-
         this.sections = this._getSections();
-
         this._setEvents();
+
     }
+
+
     /**
      * Build HTML
      * @private
      */
-
-
     Dics.prototype._build = function () {
         let dics = this;
 
         dics._applyGlobalClass(dics.options);
 
         let imagesLength = dics.images.length;
+
         dics.options.container.style.height = `${dics._calcContainerHeight()}px`;
+
         let initialImagesContainerWidth = dics.container[dics.config.offsetSizeField] / imagesLength;
 
         for (let i = 0; i < imagesLength; i++) {
-            let image = dics.images[i];
-
-            let section = dics._createElement('div', 'b-dics__section');
-
+            let image          = dics.images[i];
+            let section        = dics._createElement('div', 'b-dics__section');
             let imageContainer = dics._createElement('div', 'b-dics__image-container');
-
-            let slider = dics._createElement('div', 'b-dics__slider');
+            let slider         = dics._createElement('div', 'b-dics__slider');
 
             dics._applyFilter(image, i, dics.options.filters);
-
             dics._createAltText(image, imageContainer);
-
             dics._rotate(image, imageContainer);
 
+
             section.setAttribute('data-function', 'b-dics__section');
-            section.style[this.config.sizeField] = `${initialImagesContainerWidth}px`;
+            section.style[this.config.sizeField]    = `${initialImagesContainerWidth}px`;
             slider.style[this.config.positionField] = `${initialImagesContainerWidth * (i + 1)}px`;
+
             this.sliders.push(slider);
+
             image.classList.add('b-dics__image');
+
             section.appendChild(imageContainer);
             imageContainer.appendChild(image);
 
@@ -93,29 +94,32 @@
             }
 
             dics.container.appendChild(section);
-            image.style[this.config.positionField] = `${i * -initialImagesContainerWidth}px`;
+
+            image.style[this.config.positionField] = `${ i * -initialImagesContainerWidth }px`;
+
         }
     };
+
+
     /**
      *
      * @returns {NodeListOf<SVGElementTagNameMap[string]> | NodeListOf<HTMLElementTagNameMap[string]> | NodeListOf<Element>}
      * @private
      */
-
-
     Dics.prototype._getImages = function () {
         return this.container.querySelectorAll('img');
     };
+
+
     /**
      *
      * @returns {NodeListOf<SVGElementTagNameMap[string]> | NodeListOf<HTMLElementTagNameMap[string]> | NodeListOf<Element>}
      * @private
      */
-
-
     Dics.prototype._getSections = function () {
         return this.container.querySelectorAll('[data-function="b-dics__section"]');
     };
+
     /**
      *
      * @param elementClass
@@ -123,19 +127,18 @@
      * @returns {HTMLElement | HTMLSelectElement | HTMLLegendElement | HTMLTableCaptionElement | HTMLTextAreaElement | HTMLModElement | HTMLHRElement | HTMLOutputElement | HTMLPreElement | HTMLEmbedElement | HTMLCanvasElement | HTMLFrameSetElement | HTMLMarqueeElement | HTMLScriptElement | HTMLInputElement | HTMLUnknownElement | HTMLMetaElement | HTMLStyleElement | HTMLObjectElement | HTMLTemplateElement | HTMLBRElement | HTMLAudioElement | HTMLIFrameElement | HTMLMapElement | HTMLTableElement | HTMLAnchorElement | HTMLMenuElement | HTMLPictureElement | HTMLParagraphElement | HTMLTableDataCellElement | HTMLTableSectionElement | HTMLQuoteElement | HTMLTableHeaderCellElement | HTMLProgressElement | HTMLLIElement | HTMLTableRowElement | HTMLFontElement | HTMLSpanElement | HTMLTableColElement | HTMLOptGroupElement | HTMLDataElement | HTMLDListElement | HTMLFieldSetElement | HTMLSourceElement | HTMLBodyElement | HTMLDirectoryElement | HTMLDivElement | HTMLUListElement | HTMLHtmlElement | HTMLAreaElement | HTMLMeterElement | HTMLAppletElement | HTMLFrameElement | HTMLOptionElement | HTMLImageElement | HTMLLinkElement | HTMLHeadingElement | HTMLSlotElement | HTMLVideoElement | HTMLBaseFontElement | HTMLTitleElement | HTMLButtonElement | HTMLHeadElement | HTMLParamElement | HTMLTrackElement | HTMLOListElement | HTMLDataListElement | HTMLLabelElement | HTMLFormElement | HTMLTimeElement | HTMLBaseElement}
      * @private
      */
-
-
     Dics.prototype._createElement = function (elementClass, className) {
         let newElement = document.createElement(elementClass);
+
         newElement.classList.add(className);
+
         return newElement;
     };
+
     /**
      * Set need DOM events
      * @private
      */
-
-
     Dics.prototype._setEvents = function () {
         let dics = this;
 
@@ -143,19 +146,20 @@
 
         let listener = function (event) {
             let position = dics._calcPosition(event);
+            if (position < (dics.sections[dics._activeSlider + 1][dics.config.offsetPositionField] + dics.sections[dics._activeSlider + 1][dics.config.offsetSizeField]) && (dics._activeSlider === 0 || position > (dics.sections[dics._activeSlider - 1][dics.config.offsetPositionField] + dics.sections[dics._activeSlider - 1][dics.config.offsetSizeField]))) {
 
-            if (position < dics.sections[dics._activeSlider + 1][dics.config.offsetPositionField] + dics.sections[dics._activeSlider + 1][dics.config.offsetSizeField] && (dics._activeSlider === 0 || position > dics.sections[dics._activeSlider - 1][dics.config.offsetPositionField] + dics.sections[dics._activeSlider - 1][dics.config.offsetSizeField])) {
                 let beforeSectionsWidth = dics._beforeSectionsWidth(dics.sections, dics.images, dics._activeSlider);
 
                 dics.sliders[dics._activeSlider].style[dics.config.positionField] = `${position}px`;
-                let calcMovePixels = position - beforeSectionsWidth;
-                dics.sections[dics._activeSlider].style[dics.config.sizeField] = `${calcMovePixels}px`;
-                dics.sections[dics._activeSlider + 1].style[dics.config.sizeField] = `${dics._beforeNextWidth - (calcMovePixels - dics._beforeActiveWidth)}px`;
+
+                let calcMovePixels                                                 = position - beforeSectionsWidth;
+                dics.sections[dics._activeSlider].style[dics.config.sizeField]     = `${calcMovePixels}px`;
+                dics.sections[dics._activeSlider + 1].style[dics.config.sizeField] = `${dics._beforeNextWidth - (calcMovePixels - dics._beforeActiveWidth) }px`;
 
                 dics._setLeftToImages(dics.sections, dics.images);
-
                 dics._slidesFollowSections(dics.sections, dics.sliders);
             }
+
         };
 
         dics.container.addEventListener('click', listener);
@@ -163,13 +167,15 @@
         for (let i = 0; i < dics.sliders.length; i++) {
             let slider = dics.sliders[i];
             utils.setMultiEvents(slider, ['mousedown', 'touchstart'], function () {
-                dics._activeSlider = i;
+                dics._activeSlider      = i;
                 dics._beforeActiveWidth = dics.sections[i].getBoundingClientRect()[dics.config.sizeField];
-                dics._beforeNextWidth = dics.sections[i + 1].getBoundingClientRect()[dics.config.sizeField];
+                dics._beforeNextWidth   = dics.sections[i + 1].getBoundingClientRect()[dics.config.sizeField];
                 slider.classList.add('b-dics__slider--active');
+
                 utils.setMultiEvents(dics.container, ['mousemove', 'touchmove'], listener);
             });
         }
+
 
         let listener2 = function () {
             let activeElements = dics.container.querySelectorAll('.b-dics__slider--active');
@@ -181,10 +187,15 @@
         };
 
         utils.setMultiEvents(document.body, ['mouseup', 'touchend'], listener2);
+
+
         utils.setMultiEvents(window, ['resize', 'load'], function () {
             dics._setImageSize();
         });
+
+
     };
+
     /**
      *
      * @param sections
@@ -193,77 +204,68 @@
      * @returns {number}
      * @private
      */
-
-
     Dics.prototype._beforeSectionsWidth = function (sections, images, activeSlider) {
         let width = 0;
-
         for (let i = 0; i < sections.length; i++) {
             let section = sections[i];
-
             if (i !== activeSlider) {
                 width += section.getBoundingClientRect()[this.config.sizeField];
             } else {
-                return width;
+                return width
             }
         }
     };
+
     /**
      *
      * @returns {number}
      * @private
      */
-
-
     Dics.prototype._calcContainerHeight = function () {
-        let imgHeight = this.images[0].clientHeight;
-        let imgWidth = this.images[0].clientWidth;
+        let imgHeight      = this.images[0].clientHeight;
+        let imgWidth       = this.images[0].clientWidth;
         let containerWidth = this.options.container.getBoundingClientRect().width;
-        return containerWidth / imgWidth * imgHeight;
+
+        return (containerWidth / imgWidth) * imgHeight;
     };
+
     /**
      *
      * @param sections
      * @param images
      * @private
      */
-
-
     Dics.prototype._setLeftToImages = function (sections, images) {
         let width = 0;
-
         for (let i = 0; i < images.length; i++) {
             let image = images[i];
+
             image.style[this.config.positionField] = `-${width}px`;
             width += sections[i].getBoundingClientRect()[this.config.sizeField];
         }
     };
+
     /**
      *
      * @param sections
      * @param sliders
      * @private
      */
-
-
     Dics.prototype._slidesFollowSections = function (sections, sliders) {
         let left = 0;
-
         for (let i = 0; i < sections.length; i++) {
             let section = sections[i];
             left += section.getBoundingClientRect()[this.config.sizeField];
-
             if (i === this._activeSlider) {
                 sliders[i].style[this.config.positionField] = `${left}px`;
             }
         }
     };
+
     /**
      *
      * @private
      */
-
-
     Dics.prototype._disableImageDrag = function () {
         for (let i = 0; i < this.images.length; i++) {
             this.sliders[i].addEventListener('dragstart', function (e) {
@@ -274,6 +276,7 @@
             });
         }
     };
+
     /**
      *
      * @param image
@@ -281,22 +284,20 @@
      * @param filters
      * @private
      */
-
-
     Dics.prototype._applyFilter = function (image, index, filters) {
         if (filters) {
             image.style.filter = filters[index];
         }
     };
+
     /**
      *
      * @param options
      * @private
      */
-
-
     Dics.prototype._applyGlobalClass = function (options) {
         let container = options.container;
+
         container.classList.add('b-dics');
 
         if (options.hideTexts) {
@@ -304,55 +305,56 @@
         }
 
         if (options.linesOrientation === 'vertical') {
-            container.classList.add('b-dics--vertical');
+            container.classList.add('b-dics--vertical')
         }
 
         if (options.textPosition === 'center') {
-            container.classList.add('b-dics--tp-center');
+            container.classList.add('b-dics--tp-center')
         } else if (options.textPosition === 'bottom') {
-            container.classList.add('b-dics--tp-bottom');
+            container.classList.add('b-dics--tp-bottom')
         } else if (options.textPosition === 'left') {
-            container.classList.add('b-dics--tp-left');
+            container.classList.add('b-dics--tp-left')
         } else if (options.textPosition === 'right') {
-            container.classList.add('b-dics--tp-right');
+            container.classList.add('b-dics--tp-right')
         }
     };
+
+
     /**
      *
      * @param image
      * @param imageContainer
      * @private
      */
-
-
     Dics.prototype._createAltText = function (image, imageContainer) {
         let textContent = image.getAttribute('alt');
-
         if (textContent) {
             let text = this._createElement('p', 'b-dics__text');
 
             text.appendChild(document.createTextNode(textContent));
+
             imageContainer.appendChild(text);
         }
     };
+
+
     /**
      *
      * @param image
      * @param imageContainer
      * @private
      */
-
-
     Dics.prototype._rotate = function (image, imageContainer) {
-        image.style.rotate = `-${this.options.rotate}`;
+        image.style.rotate          = `-${this.options.rotate}`;
         imageContainer.style.rotate = this.options.rotate;
+
     };
+
+
     /**
      *
      * @private
      */
-
-
     Dics.prototype._removeActiveElements = function () {
         let activeElements = Dics.container.querySelectorAll('.b-dics__slider--active');
 
@@ -361,61 +363,66 @@
             utils.removeMultiEvents(Dics.container, ['mousemove', 'touchmove'], Dics.prototype._removeActiveElements);
         }
     };
+
+
     /**
      *
      * @param linesOrientation
      * @private
      */
-
-
     Dics.prototype._setOrientation = function (linesOrientation) {
         this.config = {};
 
         if (linesOrientation === 'vertical') {
-            this.config.offsetSizeField = 'offsetHeight';
+            this.config.offsetSizeField     = 'offsetHeight';
             this.config.offsetPositionField = 'offsetTop';
-            this.config.sizeField = 'height';
-            this.config.positionField = 'top';
-            this.config.clientField = 'clientY';
-            this.config.pageField = 'pageY';
+            this.config.sizeField           = 'height';
+            this.config.positionField       = 'top';
+            this.config.clientField         = 'clientY';
+            this.config.pageField           = 'pageY';
         } else {
-            this.config.offsetSizeField = 'offsetWidth';
+            this.config.offsetSizeField     = 'offsetWidth';
             this.config.offsetPositionField = 'offsetLeft';
-            this.config.sizeField = 'width';
-            this.config.positionField = 'left';
-            this.config.clientField = 'clientX';
-            this.config.pageField = 'pageX';
+            this.config.sizeField           = 'width';
+            this.config.positionField       = 'left';
+            this.config.clientField         = 'clientX';
+            this.config.pageField           = 'pageX';
         }
+
+
     };
+
+
     /**
      *
      * @param event
      * @returns {number}
      * @private
      */
-
-
     Dics.prototype._calcPosition = function (event) {
         let containerCoords = this.container.getBoundingClientRect();
-        let pixel = !isNaN(event[this.config.clientField]) ? event[this.config.clientField] : event.touches[0][this.config.clientField];
+        let pixel           = !isNaN(event[this.config.clientField]) ? event[this.config.clientField] : event.touches[0][this.config.clientField];
+
         return containerCoords[this.config.positionField] < pixel ? pixel - containerCoords[this.config.positionField] : 0;
     };
+
+
     /**
      *
      * @private
      */
-
-
     Dics.prototype._setImageSize = function () {
         this.images[0].style[this.config.sizeField] = this.container[this.config.offsetSizeField] + 'px';
     };
+
+
     /**
      *
      * @type {{extend: (function(*=, *, *): *), setMultiEvents: setMultiEvents, removeMultiEvents: removeMultiEvents, getConstructor: (function(*=): string)}}
      */
-
-
     let utils = {
+
+
         /**
          * Native extend object
          * @param target
@@ -424,6 +431,7 @@
          * @returns {*}
          */
         extend: function (target, objects, options) {
+
             for (let object in objects) {
                 if (objects.hasOwnProperty(object)) {
                     recursiveMerge(target, objects[object]);
@@ -434,21 +442,19 @@
                 for (let property in object) {
                     if (object.hasOwnProperty(property)) {
                         let current = object[property];
-
                         if (utils.getConstructor(current) === 'Object') {
                             if (!target[property]) {
                                 target[property] = {};
                             }
-
                             recursiveMerge(target[property], current);
-                        } else {
+                        }
+                        else {
                             // clearEmpty
                             if (options.clearEmpty) {
                                 if (current == null) {
                                     continue;
                                 }
                             }
-
                             target[property] = current;
                         }
                     }
@@ -457,6 +463,7 @@
 
             return target;
         },
+
 
         /**
          * Set Multi addEventListener
@@ -470,6 +477,7 @@
             }
         },
 
+
         /**
          *
          * @param element
@@ -482,6 +490,7 @@
             }
         },
 
+
         /**
          * Get object constructor
          * @param object
@@ -492,11 +501,17 @@
         }
     };
 
+
     if (typeof define === 'function' && define.amd) {
         define('Dics', [], function () {
             return Dics;
         });
-    } else {
+    }
+    else {
         root.Dics = Dics;
     }
-})(this);
+
+
+}(this));
+
+
